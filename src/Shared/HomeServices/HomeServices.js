8db27c5e1dbe07b954b733b70eb4ce7cbf98096a
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-11-08 04:49:11 +0600

    card added

--- src/Shared/HomeServices/HomeServices.js
+++ src/Shared/HomeServices/HomeServices.js
@@ -2,17 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 const HomeServices = () => {
 
-    const [card, setCart] = useState([])
+    const [cards, setCards] = useState([])
 
     useEffect(()=>{
         fetch('http://localhost:5300')
         .then(res => res.json())
-        .then(data => setCart(data))
+        .then(data => setCards(data))
     }, [])
 
     return (
         <div>
-            <h2>card have {card.length}</h2>
+            <h2 className='text-4xl font-bold text-center'>See all of our services here.</h2>
+
+           <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10'>
+           {
+                cards.map(card=> 
+                <div className="card  bg-base-100 shadow-xl">
+                <figure className="p-5">
+                  <img src={card.image} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">{card.title}</h2>
+                  <p>{card.description}</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+              )}
+           </div>
             
         </div>
     );
